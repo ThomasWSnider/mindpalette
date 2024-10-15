6f3e99f707f53675f679d4dfe209f3d893e4fb50
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-10-15 14:02:25 -0600

    main content card

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,34 +3,20 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="content-container container shadow rounded">
+
   </div>
 </template>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.content-container {
+  background-color: #FFFFFF;
+  height: 70vh;
+  max-width: 100vw;
+  width: 70vw;
+  position: absolute;
+  top: 56%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
