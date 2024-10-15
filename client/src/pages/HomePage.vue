67c2c815ebb0a371e11f1f3edd85146b1c76b697
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-10-15 14:50:31 -0600

    scaffolded card content

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,16 +4,20 @@
 
 <template>
   <div class="content-container container shadow rounded">
-
+    <div class="row">
+      <div class="col-12">
+        <p class="fs-3 fw-semibold mt-2 mb-5 ms-2">Decks</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .content-container {
   background-color: #FFFFFF;
-  height: 70vh;
+  height: 68vh;
   max-width: 100vw;
-  width: 70vw;
+  width: 53vw;
   position: absolute;
   top: 56%;
   left: 50%;
